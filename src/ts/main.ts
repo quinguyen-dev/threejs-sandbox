78536fc787ee8283af0ec83cf891a566ec83c150
Qui Nguyen <quinguyen.dev@gmail.com>
2023-06-15 12:01:54 -0500

    [✨] Spinning cube with some shadows.

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,27 +1,34 @@
-import "../scss/main.scss";
+import "/src/scss/main.scss";
 
 import * as THREE from "three";
 
 /* Create essential three components */
+const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-    75 /* Field of view */,
-    window.innerWidth / window.innerHeight /* Aspect ratio */,
-    0.1 /* Near clipping plane */,
-    1000 /* Far clipping plane */
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
 );
-const renderer = new THREE.WebGLRenderer();
+camera.position.z = 5;
 
+/* Set the size of the renderer to the eire window */
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+/* Create pointlight */
+const light = new THREE.PointLight(0xffffff, 1);
+light.position.set(200, 200, 200);
+scene.add(light);
+
+/* Create cube */
+let geometry = new THREE.BoxGeometry(1, 1, 1);
+let material = new THREE.MeshPhysicalMaterial();
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
-
+/* Animate the cube every frame */
 function animate() {
     requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
